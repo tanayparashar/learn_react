ae544d621b053d8b6030d76826a4e44df92336b2
tanayparashar <tanay.parashar@outlook.com>
2021-05-18 01:26:02 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+import React from "react";
+import ReactDOM from "react-dom";
+//import Pet from "./Pet";
+import SearchParams from "./SearchParams"
+const App =() =>{
+  return(
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams/>
+    </div>
+  );
 };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
